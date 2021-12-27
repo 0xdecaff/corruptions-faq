00967541907f18e0f3d3b4ae5817e5f8b29758c7
0xdecaff <0xdecaff@protonmail.com>
2021-12-27 09:35:36 -0700

    Modify records from generated

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,21 @@
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
-  Record as RecordEvent,
-  Revoke as RevokeEvent
+  RecordCall,
+  RevokeCall
 } from "../generated/CorruptionsFAQ/CorruptionsFAQ"
 import { Record, Revoke } from "../generated/schema"
 
-export function handleRecord(event: RecordEvent): void {
-  let entity = new Record(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.topic = event.params.topic
-  entity.content = event.params.content
-  entity.save()
+export function handleRecord(call: RecordCall): void {
+  let entity = new Record(call.transaction.hash.toHexString());
+  entity.created = call.block.timestamp;
+  entity.topic = call.inputs.topic;
+  entity.content = call.inputs.content;
+  entity.save();
 }
 
-export function handleRevoke(event: RevokeEvent): void {
-  let entity = new Revoke(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.topic = event.params.topic
-  entity.save()
+export function handleRevoke(call: RevokeCall): void {
+  let entity = new Revoke(call.transaction.hash.toHexString());
+  entity.created = call.block.timestamp;
+  entity.topic = call.inputs.topic;
+  entity.save();
 }
